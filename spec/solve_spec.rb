a71c94f883367f3019834f9ca1a39c6586af3595
Adeshina Adenuga <Nuga0909@gmail.com>
2023-11-16 13:52:54 +0100

    add test for fizzbbuzz method

--- spec/solve_spec.rb
+++ spec/solve_spec.rb
@@ -17,9 +17,31 @@ describe Solve do
 
     describe "#reverse" do
         it "returns the reversed word" do
-            expect(reverse("hello")).to eq("olleh")
-            expect(reverse("world")).to eq("dlrow")
-            expect(reverse("12345")).to eq("54321")
+            expect(subject.reverse("hello")).to eq("olleh")
+            expect(subject.reverse("world")).to eq("dlrow")
+            expect(subject.reverse("12345")).to eq("54321")
         end
     end
+
+  describe "#fizzbuzz" do
+    it "returns 'fizz' when given a multiple of 3" do
+      expect(subject.fizzbuzz(3)).to eq("fizz")
+      expect(subject.fizzbuzz(6)).to eq("fizz")
+    end
+
+    it "returns 'buzz' when given a multiple of 5" do
+      expect(subject.fizzbuzz(5)).to eq("buzz")
+      expect(subject.fizzbuzz(10)).to eq("buzz")
+    end
+
+    it "returns 'fizzbuzz' when given a multiple of 3 and 5" do
+      expect(subject.fizzbuzz(15)).to eq("fizzbuzz")
+      expect(subject.fizzbuzz(30)).to eq("fizzbuzz")
+    end
+
+    it "returns the given number as a string in any other case" do
+      expect(subject.fizzbuzz(7)).to eq("7")
+      expect(subject.fizzbuzz(11)).to eq("11")
+    end
+  end
 end
