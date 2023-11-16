7f62cae277adb011c7d02eb1f79d82e50d86a65b
Adeshina Adenuga <Nuga0909@gmail.com>
2023-11-16 14:13:43 +0100

    fix linter errors

--- spec/solve_spec.rb
+++ spec/solve_spec.rb
@@ -1,27 +1,27 @@
 require './solve'
 
 describe Solve do
-    describe "#factorial" do
-        it "returns the factorial of a positive integer" do
-        expect(subject.factorial(5)).to eq(120)
-        end
-
-        it "returns 1 for 0" do
-        expect(subject.factorial(0)).to eq(1)
-        end
-
-        it "raises an exception for negative integers" do
-        expect { subject.factorial(-5) }.to raise_error(ArgumentError)
-        end
+  describe "#factorial" do
+    it "returns the factorial of a positive integer" do
+      expect(subject.factorial(5)).to eq(120)
     end
 
-    describe "#reverse" do
-        it "returns the reversed word" do
-            expect(subject.reverse("hello")).to eq("olleh")
-            expect(subject.reverse("world")).to eq("dlrow")
-            expect(subject.reverse("12345")).to eq("54321")
-        end
+    it "returns 1 for 0" do
+      expect(subject.factorial(0)).to eq(1)
     end
+
+    it "raises an exception for negative integers" do
+      expect { subject.factorial(-5) }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#reverse" do
+    it "returns the reversed word" do
+      expect(subject.reverse("hello")).to eq("olleh")
+      expect(subject.reverse("world")).to eq("dlrow")
+      expect(subject.reverse("12345")).to eq("54321")
+    end
+  end
 
   describe "#fizzbuzz" do
     it "returns 'fizz' when given a multiple of 3" do
